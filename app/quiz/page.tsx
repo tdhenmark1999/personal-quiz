70c3a7d3ee6c316eab713f735f44e3073c07c921
tdhenmark1999 <torreno.mark1999@gmail.com>
2024-10-03 18:29:11 +0800

    feat: create initial structure for QuizPage with state

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const questions = [
+  { question: "You enjoy vibrant social events with lots of people.", options: ["Agree", "Neutral", "Disagree"] },
+ 
+];
 
 const QuizPage = () => {
-   
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const router = useRouter();
+
   return (
-    <div>
-        <p>Quiz page</p>
+    <div className="min-h-screen flex items-center justify-center">
+     
     </div>
   );
 };
